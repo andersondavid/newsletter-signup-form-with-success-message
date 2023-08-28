60567faafae449fffa025cdabacc746d5f63a08d
Anderson David SF <andersondavidprofissional@gmail.com>
2023-08-27 23:30:37 -0300

    feat: theme configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      tomato: "hsl(4, 100%, 67%)",
+      darkslategrey: "hsl(234, 29%, 20%)",
+      charcoalgrey: "hsl(235, 18%, 26%)",
+      grey: "hsl(231, 7%, 60%)",
+      white: "hsl(0, 0%, 100%)",
     },
+    fontFamily: {
+      ubuntu: ["Ubuntu", "sans-serif"],
+    },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
