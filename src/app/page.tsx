8c872055a3dbec43e7e95c2c4d407c91cbd84648
Anderson David SF <andersondavidprofissional@gmail.com>
2023-08-27 23:31:01 -0300

    feat: top banner and texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,63 @@
-import Image from 'next/image'
+import Image from "next/image";
+import EmailTextInput from "./components/EmailTextInput";
 
 export default function Home() {
   return (
     <main className="">
-     
+      <div className="w-full flex">
+        <Image
+          alt=""
+          width={100}
+          height={100}
+          style={{ height: "auto", width: "100%" }}
+          fill={false}
+          src="/assets/images/illustration-sign-up-mobile.svg"
+        />
+      </div>
+      <div className="px-6">
+        <h1 className="font-ubuntu text-4xl pt-10 pb-5">Stay uptated!</h1>
+        <p className="font-ubuntu">
+          Join 60,000+ product managers receiving monthly updates on:
+        </p>
+      </div>
+      <div className="px-6 pt-5">
+        <div className="flex mt-3">
+          <picture>
+            <Image
+              src="../assets/images/icon-list.svg"
+              width="22"
+              height="22"
+              alt="icon list"
+            />
+          </picture>
+          <p className="pl-4">Product discovery and build what matters</p>
+        </div>
+        <div className="flex mt-3">
+          <picture>
+            <Image
+              src="../assets/images/icon-list.svg"
+              width="22"
+              height="22"
+              alt="icon list"
+            />
+          </picture>
+          <p className="pl-4">Meassuring to ensure updates are a success</p>
+        </div>
+        <div className="flex mt-3">
+          <picture>
+            <Image
+              src="../assets/images/icon-list.svg"
+              width="22"
+              height="22"
+              alt="icon list"
+            />
+          </picture>
+          <p className="pl-4">And much more!</p>
+        </div>
+      </div>
+      <div className="px-6 pt-6">
+        <EmailTextInput />
+      </div>
     </main>
-  )
+  );
 }
